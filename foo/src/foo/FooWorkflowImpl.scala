db206b260b1022d447ba19e2290656bc6e9efc8a
Tom Crockett <pelotom@gmail.com>
2013-04-05 01:00:42 -0700

    Make more idiomatic

--- foo/src/foo/FooWorkflowImpl.scala
+++ foo/src/foo/FooWorkflowImpl.scala
@@ -5,14 +5,20 @@ import scalaz._
 import Scalaz._
 import scalaz.Monad._
 import std.indexedSeq._
+import com.amazonaws.services.simpleworkflow.flow.core.Promise
 
 class FooWorkflowImpl extends FooWorkflow {
+  
   val activities = new FooActivitiesClientImpl();
 
-  override def startFoo() = for (i <- 1 to 20) {
+  override def startFoo() {
     for {
-      name 	<- activities.getName(i)
-      _		<- activities.printGreeting(s"Hello $name!")
-    }()
+      names <- 1 to 20 traverse greet
+    } yield println(s"All done: ${names.length} greetings delivered")
   }
+  
+  def greet(i:Int): Promise[String] = for {
+    name 	<- activities getName i
+    _ 		<- activities printGreeting s"Hello $name!"
+  } yield name
 }
